1e22a4e2fb3de1bac47397b91dd6d4c4695728ae
tolga <tolga.sofuoglu@theappbusiness.com>
2017-05-14 09:04:31 +0100

    render html

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.sendFile('index.html', { root: __dirname });
 });
 
 const port = 3000;
