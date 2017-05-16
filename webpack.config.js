e45bad244fbecbedf1bdda8d3786bc0bbcc5d77b
tolga <tolga.sofuoglu@theappbusiness.com>
2017-05-17 00:06:42 +0300

    moving bundle.js to assets folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    context: __dirname + '/app',
-    entry: './index.js',
+    context: __dirname + "/app",
+    entry: "./index.js",
     output: {
-        path: __dirname + '/app',
-        filename: 'bundle.js'
+        path: __dirname + "/assets",
+        filename: "bundle.js"
     }
 };
